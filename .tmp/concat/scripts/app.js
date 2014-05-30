b1b8750352da0c1cc89b1eb51ab065671942af98
Sean Brewer <seabre986@gmail.com>
2014-05-30 17:41:37 -0400

    Initial page commit.

---
+++ .tmp/concat/scripts/app.js
@@ -0,0 +1,144 @@
+(function() {
+  'use strict';
+
+  var projection = d3.geo.mercator()
+      .scale(5500)
+      .center([-85.9, 35.2461]);
+
+  var path = d3.geo.path()
+      .projection(projection);
+
+  function dataRanges(){
+    return $.map($(new Array(8)), function(val, i) { return Math.round((i * 80/7.0) + 20); });
+  }
+
+  function graduationType(v){
+    var n = parseInt(v);
+
+    if (n === 0) {
+      return 'tn_grad_rate_district_2012_all_grad_rate';
+    } else if (n === 1) {
+      return 'tn_grad_rate_district_2012_white_grad_rate';
+    } else if (n === 2) {
+      return 'tn_grad_rate_district_2012_african_american_grad_ate';
+    } else if (n === 3) {
+      return 'tn_grad_rate_district_2012_hispanic_grad_rate';
+    } else if (n === 4) {
+      return 'tn_grad_rate_district_2012_asian_grad_rate';
+    } else if (n === 5) {
+      return 'tn_grad_rate_district_2012_native_american_grad_rate';
+    } else if (n === 6) {
+      return 'tn_grad_rate_district_2012_male_grad_rate';
+    } else if (n === 7) {
+      return 'tn_grad_rate_district_2012_female_grad_rate';
+    } else if (n === 8) {
+      return 'tn_grad_rate_district_2012_economically_disadvantaged_grad_rate';
+    } else if (n === 9) {
+      return 'tn_grad_rate_district_2012_students_with_disabilities_grad_rate';
+    } else if (n === 10) {
+      return 'tn_grad_rate_district_2012_limited_english_proficient_grad_rate';
+    }
+  }
+
+  function dataClass(d) {
+    var ranges = dataRanges();
+
+    if (isNaN(d)) {
+      return 'cc-null';
+    } else if (d >= ranges[0] && d < ranges[1]) {
+      return 'cc1';
+    } else if (d >= ranges[1] && d < ranges[2]) {
+      return 'cc2';
+    } else if (d >= ranges[2] && d < ranges[3]) {
+      return 'cc3';
+    } else if (d >= ranges[3] && d < ranges[4]) {
+      return 'cc4';
+    } else if (d >= ranges[4] && d < ranges[5]) {
+      return 'cc5';
+    } else if (d >= ranges[5] && d < ranges[6]) {
+      return 'cc6';
+    } else if (d >= ranges[6] && d <= ranges[7]) {
+      return 'cc7';
+    }
+  }
+
+  function populateInfo(t) {
+    var percentage = t.properties[graduationType($('#graduation_type').val())];
+
+    $('#school_district').text(t.properties.NAME);
+    if (percentage === '-'){
+      $('#rate').text('No Data');
+    } else {
+      $('#rate').text(percentage + '%');
+    }
+  }
+
+  function mouseover(d) {
+    populateInfo(d);
+
+    // jshint validthis: true
+    d3.select(this).style('filter', 'url(#drop_shadow)');
+  }
+
+  function mouseout() {
+    // jshint validthis: true
+    d3.select(this).style('filter', null);
+  }
+
+  function sizeChange() {
+    d3.select('g').attr('transform', 'scale(' + $('#chart').width()/900 + ')');
+    $('svg').height($('#chart').width()*0.618);
+  }
+
+  var svg = d3.select('#chart')
+    .append('svg')
+    .append('g');
+
+  var defs = svg.append('defs');
+
+  var filter = defs.append('filter')
+      .attr('id', 'drop_shadow')
+      .attr('height', '300%');
+
+  filter.append('feGaussianBlur')
+      .attr('in', 'SourceAlpha')
+      .attr('stdDeviation', 5)
+      .attr('result', 'blur');
+
+  filter.append('feOffset')
+      .attr('in', 'blur')
+      .attr('dx', 0)
+      .attr('dy', 0)
+      .attr('result', 'offsetBlur');
+
+  var feMerge = filter.append('feMerge');
+
+  feMerge.append('feMergeNode')
+      .attr('in', 'offsetBlur');
+
+  feMerge.append('feMergeNode')
+      .attr('in', 'SourceGraphic');
+
+  d3.select(window)
+    .on('resize', sizeChange);
+
+  d3.json('data/tennessee_school_districts.topojson', function(error, tn) {
+    svg.selectAll('path')
+      //jshint camelcase: false
+      .data(topojson.feature(tn, tn.objects.tennessee_school_districts).features)
+    .enter().append('path')
+      .attr('class', function(d) { return 'district ' + dataClass(d.properties[graduationType(0)]); })
+      .attr('d', path)
+      .on('mouseover', mouseover)
+      .on('mouseout', mouseout);
+  });
+
+  $('#graduation_type').change(function() {
+    svg.selectAll('path')
+    .attr('class', function(d) { return 'district ' + dataClass(d.properties[graduationType($('#graduation_type').val())]); });
+  });
+
+  $(window).load(function() {
+    sizeChange();
+  });
+})();
